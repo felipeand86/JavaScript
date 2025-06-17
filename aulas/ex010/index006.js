504c9bf4243c200c4779f8da59968a49ad76c432
Felipe André <felipe.tecpro26@gmail.com>
2025-06-17 00:28:51 -0300

    exercicio multas

---
+++ aulas/ex010/index006.js
@@ -0,0 +1,5 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel} por hora`)
+if (vel > 60){ //condição simples, pois só tem "if"
+console.log('Você está acima da velocidade, Multado!')
+}
